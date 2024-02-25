6387a014097b7f8bedb879d6cfb8510201e0530d
ambirlanga <71774915+ambirlanga@users.noreply.github.com>
2024-02-25 22:06:58 +0100

    Ariel Task 5 part added

---
+++ script/task5/Task5PDF.py
@@ -0,0 +1,56 @@
+import re
+import tika
+from tika import parser
+import pandas as pd
+import numpy as np
+
+tika.initVM()
+parsed = parser.from_file('data/population-density-data-table.pdf')
+
+x = parsed["content"]
+start = [m.start() for m in re.finditer('Alabama', x)]
+end = [m.start() for m in re.finditer('United States', x)]
+
+data = ""
+for i in range(4):
+    data += x[start[i]:end[i]]
+
+data = data.replace("\n\n", " ")
+data = data.replace("\n", " ")
+data = data.replace(",", "")
+data = data.replace("District of ", "")
+data = data.replace("New ", "New_")
+data = data.replace("North ", "North_")
+data = data.replace("South ", "South_")
+data = data.replace("West ", "West_")
+data = data.replace("Rhode ", "Rhode_")
+data = data.split(' ')
+  
+chunks = [data[x:x+10] for x in range(0, len(data)-1, 10)]
+complete = []
+for i in range(51):
+    name = chunks[51+i].pop(0)
+    chunks[102+i].pop(0)
+    chunks[153+i].pop(0)
+    if "_" in name:
+        name = name.replace("_", " ")
+    fuse = chunks[i]+chunks[51+i]+chunks[102+i]+chunks[153+i]
+    decade=0
+    for j in range(2021,1904,-1):
+        complete.append([name, j]+fuse[1+decade:4+decade])    
+        if j in range(1916, 2020, 10):
+            decade += 3
+
+            
+tsv = pd.read_table('dataset1/reports_task5.tsv', encoding='utf8')
+df = pd.DataFrame(tsv)
+
+col = ['State', 'Fixed Year', 'State Resident Population', 'State Population Density', 'Census Rank']
+df2 = pd.DataFrame(complete, columns = col) 
+
+bigfoot_df = pd.merge(
+    df,
+    df2[['State', 'Fixed Year', 'State Resident Population', 'State Population Density', 'Census Rank']],
+    left_on=['State', 'Fixed Year'], right_on=['State', 'Fixed Year'], how='left')
+
+bigfoot_df.to_csv('dataset1/reports_task5.tsv', sep="\t", index=False) 
