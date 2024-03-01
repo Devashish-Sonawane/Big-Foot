fc953c38e5d08ec2cbd46a665dca1ea867a4300f
Kyosuke Chikamatsu <chikamat@usc.edu>
2024-02-29 21:32:22 -0800

    run through task5

--- script/task5/Task5Launch.py
+++ script/task5/Task5Launch.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from Task5TXT import *
 from Task5PDF import *
-#from Task5JPG import *
+from Task5JPG import *
 
 if __name__ == "__main__":
     tsv = pd.read_table('dataset1/reports_task4-e_last.tsv', encoding='utf8')
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     tsv = merge_txt(tsv)
     tsv = merge_pdf(tsv)
-    #tsv = merge_jpg(tsv)
+    tsv = merge_jpg(tsv)
 
     tsv.to_csv('dataset1/reports_task5.tsv', sep="\t", index=False) 
     
