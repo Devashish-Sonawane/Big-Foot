cfaf80f878b43db0ef7b46f1303aa991f80b87ca
ambirlanga <71774915+ambirlanga@users.noreply.github.com>
2024-03-03 00:35:22 +0100

    Commented Task5JPG.py

--- script/task5/Task5JPG.py
+++ script/task5/Task5JPG.py
@@ -8,6 +8,7 @@ def merge_jpg(bfro_df):
     # Merge BFRO and STATEMAMMAL data on 'State' column
     merged_df = pd.merge(bfro_df, statemammal_df, on='State', how='left')
 
+    # Code for local execution testing
     # Select and reorder columns in the desired output format
     # output_columns = ['State', 'STATE MAMMAL 1', 'YEAR 1', 'SIZE 1', 'STATE MAMMAL 2', 'YEAR 2', 'SIZE 2', 'STATE MAMMAL 3', 'YEAR 3', 'SIZE 3', 'STATE MAMMAL 4', 'YEAR 4',
     #                   'SIZE 4']
@@ -16,5 +17,6 @@ def merge_jpg(bfro_df):
     # Write the final dataframe to a new TSV file
     # final_df.to_csv("/Users/willy/Desktop/Academic/usc/2024 Spring/DSCI550/output_BFRO.tsv", sep="\t", index=False)
 
+    # Return merged dataset
     return merged_df
     # print("Output written to output_BFRO.tsv")
