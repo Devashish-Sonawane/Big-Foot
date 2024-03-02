6b725754af91b133ce6997b1a8c428a561d96f5a
ambirlanga <71774915+ambirlanga@users.noreply.github.com>
2024-03-03 00:37:41 +0100

    Commented Task5Launch.py

--- script/task5/Task5Launch.py
+++ script/task5/Task5Launch.py
@@ -4,12 +4,16 @@ from Task5PDF import *
 from Task5JPG import *
 
 if __name__ == "__main__":
+
+    # Load the Bigfoot tsv (task 4 processed) into a pandas DataFrame
     tsv = pd.read_table('dataset1/reports_task4-e_last.tsv', encoding='utf8')
     tsv = pd.DataFrame(tsv)
 
+    # Execute each Task 5 script sequentially, merging each dataset one after the other
     tsv = merge_txt(tsv)
     tsv = merge_pdf(tsv)
     tsv = merge_jpg(tsv)
 
+    # Creates a tsv with the new merged data
     tsv.to_csv('dataset1/reports_task5.tsv', sep="\t", index=False) 
     
