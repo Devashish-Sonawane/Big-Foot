daedcefee75e0ec9f0af0eadb2452b80c33c1620
Kyosuke Chikamatsu <chikamat@usc.edu>
2024-04-02 16:25:11 -0700

    change Solr core name

--- Assignment_3/Scripts/task5/create-core.sh
+++ Assignment_3/Scripts/task5/create-core.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CMD='cd /home/Solr/solr-5.3.1/ && ./bin/solr create_core -c covid19'
+CMD='cd /home/Solr/solr-5.3.1/ && ./bin/solr create_core -c bigfoot'
 docker exec -it docker-memex-geoparser-1 bash -c "$CMD"
 
