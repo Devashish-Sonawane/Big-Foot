130b514b039eb01339f201f294f98bbe4a8bdf2b
Devashish Sonawane <Deva>
2024-04-25 12:48:03 -0700

    renamed vis 4 files

--- Assignment_3/Scripts/task1-2/vis4/visualization_4.py
+++ Assignment_3/Scripts/task1-2/vis4/visualization_4.py
@@ -66,5 +66,5 @@ def create_json(input_filename, output_filename):
         print(f"An error occurred: {str(e)}")
 
 input = r"..\Data\reports_v3.tsv"
-output = r"data.json"
+output = r"visualization_4.json"
 create_json(input, output)
